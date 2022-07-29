f5a6790483840b2dcd3882b21442caba570f4c72
n1colashka <deckards2505@gmail.com>
2022-07-29 21:41:31 +0600

    add modals

--- js/main.js
+++ js/main.js
@@ -192,7 +192,35 @@ window.addEventListener('DOMContentLoaded', function () {
 		})
 	}
 	
+	function initModals() {
+		const $modals = document.querySelectorAll('.modal')
+		const $modalsTriggers = document.querySelectorAll(
+			'[data-micromodal-trigger]'
+		)
+
+		$modalsTriggers.forEach(item => {
+			item.addEventListener('click', e => e.preventDefault())
+		})
+
+		if ($modals.length > 0) {
+			MicroModal.init({
+				onShow: modal => {
+					// Custom events
+				},
+				onClose: modal => {
+					// Custom events
+				},
+				disableFocus: true,
+				openClass: 'is-open',
+				awaitOpenAnimation: true,
+				awaitCloseAnimation: true,
+				disableScroll: true,
+			})
+		}
+	}
+	
 	initMenu()
+	initModals()
 	initCountdown()
 	initHeroSlider()
 	initPlayer()
